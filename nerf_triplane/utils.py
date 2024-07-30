ab67b161b59751fa6cd8066d3c12ac5de8c7b388
jiabin-GUO <96671620+kobeap@users.noreply.github.com>
2024-07-30 22:33:19 +0800

    guo
    to device

--- nerf_triplane/utils.py
+++ nerf_triplane/utils.py
@@ -801,7 +801,7 @@ class Trainer(object):
         auds = data['auds'] # [B, 29, 16] now [B,1,512]
         audact = data['auds'].clone() # [B, 29, 16]  now[8,1,512]
         if self.opt.contrast_loss:
-            audneg = data['rev_aud']  # now[8,1,512]
+            audneg = data['rev_aud'].to(self.device)  # now[8,1,512]
         index = data['index'] # [B]  第几帧
         #print('audact.shape',audact.shape)  #  [8,1,512]
         # print('audneg',audneg.shape)  #  [8,1,512]
